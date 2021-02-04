6df8fc0c112a7ad5fa0bbcfe04e2a66274b23da9
ndimqa <ndimqa@mail.ru>
2021-02-04 21:29:07 +0600

    leet code number of good pairs

---
+++ week_2/3_week2.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/number-of-good-pairs/submissions/
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0 
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if (i<j) and (nums[i] == nums[j]):
+                    count+=1
+        return count
+    

640a9f9e3ef561086464130a6dfe8b7282188529
ndimqa <ndimqa@mail.ru>
2021-02-11 23:30:36 +0600

    TSIS 3 problem 4

---
+++ week_3/4.py
@@ -0,0 +1,7 @@
+# https://informatics.mccme.ru/mod/statements/view.php?id=4163&chapterid=3850#1
+n = list(input())
+for i in range(len(n)):
+    if n[i]== '0':
+       n.remove('0')
+       n.append(0)
+    print(n[i],end = ' ')   

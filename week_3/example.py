c366d900ca16ce7d7075e690be94576a704796b9
Dimash Nuran <mac@MacBook-Air-Dimash.local>
2021-02-14 11:26:14 +0600

    deleted file

--- week_3/example.py
+++
@@ -1,8 +0,0 @@
-class Person:
-  def __init__(self, name = "NONE", age = 0):
-    self.name = name
-    self.age = age
-
-p1 = Person(input('enter name '), input('enter age '))
-print(p1.name)
-print(p1.age)
